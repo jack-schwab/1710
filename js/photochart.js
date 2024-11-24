d670217a3af223d8cc0b07490c628d77847df1af
dsunfriedman <90146615+dsunfriedman@users.noreply.github.com>
2024-11-24 12:33:16 -0500

    Began to create photo chart

---
+++ js/photochart.js
@@ -0,0 +1,13 @@
+class PhotoChart{
+    constructor(parentElement, data) {
+        this.parentElement = parentElement;
+        this.data = data;
+        this.displayData = data;
+
+        console.log(this.displayData);
+
+        this.initVis();
+    }
+
+    initVis() {}
+}
