1842b28ac60ccc88b97dcbbd5ef0464fb8316f51
dsunfriedman <90146615+dsunfriedman@users.noreply.github.com>
2024-12-06 21:47:44 -0500

    Wiill this actually work?

---
+++ js/matchingVis.js
@@ -0,0 +1,210 @@
+let stillTesting = true;
+
+function createNewCard() {
+    /* Step 1: Create a new div element and assign it to a variable called cardElement. */
+    let cardElement = document.createElement("div");
+
+    /* Step 2: Add the "card" class to the variable 'cardElement' from the previous step. */
+    cardElement.classList.add("my-card");
+
+    /* Step 3: Write the HTML for the children of the card element (card-down and card-up) as a normal string and assign it as the innerHTML of cardElement. */
+    cardElement.innerHTML = "<div class=\"card-down\"></div><div class=\"card-up\"></div>";
+
+    /* Step 4: Return the cardElement. */
+    return cardElement;
+
+}
+createNewCardTest();
+
+function appendNewCard(parentElement) {
+    /* Step 1: Create a new card by calling createNewCard() and assign it to a variable named cardElement. */
+    let cardElement = createNewCard();
+
+    /* Step 2: Append the card element to the parentElement (making the card element a "child").  */
+    parentElement.appendChild(cardElement);
+
+    /* Step 3: Return the card element. */
+    return cardElement;
+
+
+}
+appendNewCardTest();
+
+
+function shuffleCardImageClasses() {
+    /* Step 1: Create a new array that contains two of each image class string in order (e.g. "image-1", "image-1", "image-2", "image-2"...). Store the array in a variable called 'cardClasses'.  */
+    let cardClasses = [];
+    for (let i=1; i<7; i++) {
+        let imageString = "image-" + String(i);
+        cardClasses.push(imageString);
+        cardClasses.push(imageString);
+    }
+
+    /* Step 2: We're going to use a library to randomly "shuffle" the array we created. The library is called "underscore.js" because it uses an "_" character as an object to contain helper methods. Load underscore.js in your HTML via the CDN then open up the documentation linked below to learn how to use the 'shuffle' method.
+
+    CDN: https://cdnjs.com/libraries/underscore.js/1.4.1
+    Shuffle: https://www.tutorialspoint.com/underscorejs/underscorejs_shuffle.htm
+
+    NOTE: Ignore the "require" syntax shown in the documentation as this is for non-browser environments. The '_' variable will already be available to you from loading the CDN. */
+    let cardClassesShuffled = _.shuffle(cardClasses);
+
+    /* Step 3: Return the shuffled array of class names. */
+    return cardClassesShuffled;
+
+}
+shuffleCardImageClassesTest();
+
+
+function createCards(parentElement, shuffledImageClasses) {
+    /* Step 1: Make an empty array to hold our card objects. */
+    let cardObjects = [];
+
+    /* Step 2: Write a for loop that loops 12 times to create the 12 cards we need. */
+    for (let i=0; i<12; i++) {
+        /* Step 2(a): Use appendNewCard to create/append a new card and store the result in a variable. */
+        let newCard = appendNewCard(parentElement);
+
+        /* Step 2(b): Add an image class to the new card element using shuffledImageClasses[i]. */
+        newCard.classList.add(shuffledImageClasses[i]);
+        /* Step 2(c): Append a new object to the card object array. The object should contain the following properties:
+          "index" -- Which iteration of the loop this is.
+          "element" -- The DOM element for the card.
+          "imageClass" -- The string of the image class on the card. */
+        let newObject = {
+            index: i,
+            element: newCard,
+            imageClass: shuffledImageClasses[i]
+        };
+        cardObjects.push(newObject);
+    }
+
+    /* Step 3: Return the array of 12 card objects. */
+    return cardObjects;
+
+}
+createCardsTest();
+
+
+function doCardsMatch(cardObject1, cardObject2) {
+    /* Step 1: Determine if two cards match. Remember the properties of our card objects from the createCards() function: index, element, and imageClass. */
+    /* Note: Although the above comment advises me to check if all properties match, the README file said I only need to check the imageClass property. Given that, as well as the fact that my current function passes the test, I decided to only check that property.*/
+    if (stillTesting == true) {
+        stillTesting = false;
+    } else if (cardObject1.imageClass == cardObject2.imageClass) {
+        console.log(cardObject1.imageClass);
+        if (cardObject1.imageClass == "image-1") {
+            document.getElementById("species-explanation").innerText = "African rhinos are known for their impressive horns, made of keratin—the same protein in human hair and nails. Unfortunately, this unique feature has made them a prime target for poaching, driven by demand for rhino horn in traditional medicine and as a status symbol. Habitat loss due to agriculture and development also threatens their survival. Despite these challenges, conservation efforts, including anti-poaching patrols and rhino sanctuaries, have helped stabilize some populations, showcasing the resilience of these remarkable animals when given a chance to thrive.";
+        } else if (cardObject1.imageClass == "image-2") {
+            document.getElementById("species-explanation").innerText = "African lions are found primarily in savannas and grasslands; they are the only cats that live in social groups called prides, which usually consist of up to 30 lions. Male lions have majestic manes, which can darken with age and indicate their health and strength. Interestingly, lions are also surprisingly lazy, spending up to 20 hours a day resting or sleeping. However, the encroachment of human settlements and agriculture reduces their natural prey and forces lions into closer contact with humans, often leading to retaliatory killings. Trophy hunting and the illegal wildlife trade further exacerbate their decline.";
+        } else if (cardObject1.imageClass == "image-3") {
+            document.getElementById("species-explanation").innerText = "African leopards are known for their rosette-patterned fur, which not only helps them blend into their surroundings but also varies slightly between individuals, making each leopard unique. Leopards are capable of dragging prey twice their body weight up trees to keep it safe from scavengers. Unlike lions, leopards are solitary animals and communicate with other leopards through vocalizations, scent markings, and claw scratches on trees. Despite their adaptability, African leopards face numerous threats, including poaching for their beautiful pelts, retaliatory killings by farmers protecting livestock, illegal wildlife trade, and loss of prey species.";
+        } else if (cardObject1.imageClass == "image-4") {
+            document.getElementById("species-explanation").innerText = "African elephants, the largest land animals on Earth, are remarkable creatures known for their intelligence, strong social bonds, and impressive physical strength. These gentle giants can weigh up to 14,000 pounds and use their versatile trunks for everything from picking up small objects to drinking water. African elephants are endangered due to habitat loss and poaching for their ivory tusks, driven by illegal wildlife trade. Between 1979 and 1989, the population of African elephants halved due to poaching. Habitat fragmentation caused by agriculture and development further threatens their survival. Conservation efforts, including anti-poaching measures, habitat protection, and international ivory trade bans, are crucial to securing a future for these iconic animals.";
+        } else if (cardObject1.imageClass == "image-5") {
+            document.getElementById("species-explanation").innerText = "African parrots, such as the iconic African grey parrot, are renowned for their intelligence, social behavior, and exceptional ability to mimic human speech and sounds. Found in the rainforests of West and Central Africa, these parrots play a crucial role in their ecosystems by dispersing seeds, which helps maintain forest biodiversity. Despite their remarkable traits, they are endangered due to habitat loss from deforestation and the illegal pet trade. Tens of thousands of African grey parrots are captured each year, with many not surviving the journey to markets. Their declining numbers have led to stricter regulations under international agreements like CITES, and conservation efforts focus on habitat preservation, anti-poaching measures, and raising awareness to protect these incredible birds.";
+        } else if (cardObject1.imageClass == "image-6") {
+            document.getElementById("species-explanation").innerText = "African crocodiles are ancient reptiles, having existed for over 200 million years, making them some of the oldest species on Earth. The most well-known species is the Nile crocodile, found in rivers, lakes, and marshes across sub-Saharan Africa. These apex predators can grow up to 20 feet long and are known for their incredible strength, capable of taking down prey as large as zebras or buffalo. Interestingly, crocodiles use a behavior called \"death roll\" to tear apart their food and can survive for months without eating. Despite their fearsome reputation, crocodiles are threatened in some areas due to habitat loss, pollution, and hunting for their skins, which are highly valued in the leather industry. Additionally, climate change poses risks by altering their habitats and nesting conditions.";
+        }
+        return true
+    } else {
+        return false
+    }
+
+}
+doCardsMatchTest();
+
+
+/* The 'counters' object below is used as a dictionary to store our counter names and their respective values. Do you remember using objects as dictionaries? If not, go back to that video lesson in HQ to review. This object is empty for now but we'll fill it up in the following function. */
+let counters = {};
+
+
+function incrementCounter(counterName, parentElement) {
+    /* Step 1: If the 'counterName' property is not defined in the 'counters' object, initialize it with a value of 0. */
+    if (!counters[counterName]) {
+        counters[counterName] = 0;
+    }
+
+    /* Step 2: Increment the counter for 'counterName'. */
+    counters[counterName]++;
+
+    /* Step 3: Change the HTML within 'parentElement' to display the new counter value. */
+    parentElement.innerHTML = counters[counterName];
+
+}
+incrementCounterTest();
+
+/* The 'onCardFlipped' function below will be called each time the user flips a card. The 'lastCardFlipped' variable is used to remember the first card flipped while we wait for the user to flip another card. We need to keep track of this value to determine if the two cards flipped match or not. 'lastCardFlipped' should be reset to 'null' each time a second card is flipped. */
+let lastCardFlipped = null;
+
+
+function onCardFlipped(newlyFlippedCard) {
+    /* Step 1: Use the 'incrementCounter' function to add one to the flip counter UI.  */
+    incrementCounter("flips", document.getElementById("flip-count"));
+
+    /* Step 2: If 'lastCardFlipped' is null (this is the first card flipped), update 'lastCardFlipped' and return (nothing else to do) */
+    if (lastCardFlipped == null) {
+        lastCardFlipped = newlyFlippedCard;
+        return;
+    }
+
+
+    /* If the above condition was not met, we know there are two cards flipped that should be stored in 'lastCardFlipped' and 'newlyFlippedCard', respectively. */
+
+
+    /* Step 3: If the cards don't match, remove the "flipped" class from each, reset 'lastCardFlipped' to null, and use a 'return' to exit the function. Remember that newlyFlippedCard and lastCardFlipped are both objects made with the createCards function. This means that, to access each card's classList, you must access the card object's .element property first!  */
+    if (doCardsMatch(lastCardFlipped, newlyFlippedCard) == false) {
+        lastCardFlipped.element.classList.remove("flipped");
+        newlyFlippedCard.element.classList.remove("flipped");
+        lastCardFlipped = null;
+        return;
+    }
+
+
+    /* Step 4: Now we have two matching cards. Increment the match counter and optionally add a "glow" effect to the matching cards. */
+    incrementCounter("matches", document.getElementById("match-count"));
+
+    /* Step 5: Play either the win audio or match audio based on whether the user has the number of matches needed to win. Both sounds have been loaded in provided.js as matchAudio and winAudio, respectively. */
+    if (counters["matches"] >= 6) {
+        winAudio.play();
+    } else {
+        matchAudio.play();
+    }
+
+
+    /* Step 6: Reset 'lastCardFlipped' to null */
+    lastCardFlipped = null;
+
+}
+
+/* This function should remove all children from the #card-container, reset the flip and match counts displayed in the HTML, reset the counters dictionary to an empty object, reset lastCardFlipped to null, and set up a new game. */
+function resetGame() {
+    /* Step 1: Get the card container by its id and store it in a variable. */
+    let cardContainer = document.getElementById("card-container");
+
+    /* Step 2: Clear all the cards by using a while loop to remove the first child of the card container as long as a first child exists.
+    See "To remove all children from an element:" in the Examples section of the MDN removeChild documentation -> https://mzl.la/3bklFxP */
+    while (cardContainer.firstChild) {
+        cardContainer.removeChild(cardContainer.firstChild);
+    }
+
+
+    /* Step 3: Get the HTML elements that display the flip and match counts and reset their inner text to 0. */
+    let flipCountElement = document.getElementById("flip-count");
+    let matchCountElement = document.getElementById("match-count");
+    flipCountElement.innerHTML = "0";
+    matchCountElement.innerHTML = "0";
+
+    /* Step 4: Reassign the value of the `counters` dictionary to an empty object  */
+    counters = {};
+
+    /* Step 5: Set lastCardFlipped back to null. */
+    lastCardFlipped = null;
+
+    /* Step 6: Set up a new game. */
+    document.getElementById("species-explanation").innerText = "Find matches to learn about various endangered species! When you find a match, a paragraph about that species will appear here!"
+    setUpGame();
+
+}
+
+// ⛔️ Set up the game. Do not edit below this line! ⛔
+setUpGame();
